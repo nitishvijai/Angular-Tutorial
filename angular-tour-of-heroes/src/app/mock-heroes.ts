6a3ab9b736c71f4de7e974298e14c554e47f82bc
nitishv <nitishv@umich.edu>
2021-01-14 12:35:05 -0500

    Populated a list of heroes and bound them to the site

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Vijay' },
+    { id: 12, name: 'Ajith Kumar' },
+    { id: 13, name: 'Rajinikanth' },
+    { id: 14, name: 'Kamal Haasan' },
+    { id: 15, name: 'Suriya' },
+    { id: 16, name: 'Vikram' },
+    { id: 17, name: 'Dhanush' },
+    { id: 18, name: 'Vijay Sethupathi' },
+    { id: 19, name: 'Sivakarthikeyan' },
+    { id: 20, name: 'Arya' }
+];
